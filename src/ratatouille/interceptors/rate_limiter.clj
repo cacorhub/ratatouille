f601e774071c1b3d496597b6bac5c69d16d0d95e
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2023-11-20 08:58:25 -0300

    fix typo

--- src/ratatouille/interceptors/rate_limiter.clj
+++ src/ratatouille/interceptors/rate_limiter.clj
@@ -13,7 +13,7 @@
     {:name  ::rate-limit-four-per-min-based-on-ip-interceptor
      :enter (fn [{{:keys [components remote-addr]} :request :as context}]
               (let [{:keys [rate-limiter]} components]
-                (when-not (r/allow? (get @rate-limiter :1-per-min) remote-addr)
+                (when-not (r/allow? (get @rate-limiter :four-requests-per-min) remote-addr)
                   (common-error/http-friendly-exception 429
                                                         "too-many-requests"
                                                         "Too Many Requests"
