fd0b020bdff3abb40c5911e83611e9dd0b9a6338
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2023-11-20 08:56:07 -0300

    add interceptor

--- src/ratatouille/components.clj
+++ src/ratatouille/components.clj
@@ -8,6 +8,8 @@
             [ratatouille.diplomat.telegram.consumer :as diplomat.telegram.consumer]
             [common-clj.component.datomic :as component.datomic]
             [ratatouille.db.datomic.config :as datomic.config]
+            [common-clj.component.rate-limiter :as component.rate-limiter]
+            [ratatouille.interceptors.rate-limiter :as interceptors.rate-limiter]
             [ratatouille.diplomat.http-server :as diplomat.http-server]))
 
 (def system
@@ -17,6 +19,7 @@
     :http-client (component/using (component.http-client/new-http-client) [:config])
     :telegram-consumer (component/using (component.telegram.consumer/new-telegram-consumer diplomat.telegram.consumer/consumers) [:config :http-client :datomic])
     :routes (component/using (component.routes/new-routes diplomat.http-server/routes) [:config])
+    :rate-limiter (component.rate-limiter/new-rate-limiter interceptors.rate-limiter/rate-limiters-definition)
     :service (component/using (component.service/new-service) [:routes :config :datomic])))
 
 (defn start-system! []
---
+++ src/ratatouille/interceptors/rate_limiter.clj
@@ -0,0 +1,21 @@
+(ns ratatouille.interceptors.rate-limiter
+  (:require [clj-rate-limiter.core :as r]
+            [common-clj.error.core :as common-error]
+            [io.pedestal.interceptor :as pedestal.interceptor]))
+
+(def rate-limiters-definition
+  {:four-requests-per-min (r/rate-limiter-factory :memory
+                                                  :interval 60000
+                                                  :max-in-interval 4)})
+
+(def rate-limit-four-per-min-based-on-ip-interceptor
+  (pedestal.interceptor/interceptor
+    {:name  ::rate-limit-four-per-min-based-on-ip-interceptor
+     :enter (fn [{{:keys [components remote-addr]} :request :as context}]
+              (let [{:keys [rate-limiter]} components]
+                (when-not (r/allow? (get @rate-limiter :1-per-min) remote-addr)
+                  (common-error/http-friendly-exception 429
+                                                        "too-many-requests"
+                                                        "Too Many Requests"
+                                                        {:error :too-many-requests})))
+              context)}))
