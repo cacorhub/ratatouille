29a4b92ffecbeb0b06ee3b643eef4f756914e07a
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2023-11-20 09:46:09 -0300

    add integration test

--- src/ratatouille/components.clj
+++ src/ratatouille/components.clj
@@ -32,4 +32,5 @@
     :http-client (component/using (component.http-client/new-http-client) [:config])
     #_:telegram-consumer #_(component/using (component.telegram.consumer/new-mock-telegram-consumer diplomat.telegram.consumer/consumers) [:config :http-client :datomic])
     :routes (component/using (component.routes/new-routes diplomat.http-server/routes) [:config])
-    :service (component/using (component.service/new-service) [:routes :config :datomic])))
+    :rate-limiter (component.rate-limiter/new-rate-limiter interceptors.rate-limiter/rate-limiters-definition)
+    :service (component/using (component.service/new-service) [:routes :config :datomic :rate-limiter])))
