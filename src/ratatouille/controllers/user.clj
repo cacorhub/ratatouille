0dcb623d1d58b1cd7f2be111a0ca18e67e59cffd
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2023-11-21 05:33:27 -0300

    refactor code

--- src/ratatouille/controllers/user.clj
+++ src/ratatouille/controllers/user.clj
@@ -1,6 +1,5 @@
 (ns ratatouille.controllers.user
   (:require [datomic.client.api :as dl]
-            [morse.api :as morse-api]
             [ratatouille.models.user :as models.user]
             [schema.core :as s]
             [ratatouille.db.datomic.user :as database.user]
@@ -18,17 +17,11 @@
   [user-telegram-chat-id :- s/Str
    chat-id :- s/Str
    datomic-connection
-   {:keys [telegram]}]
+   config]
   (let [{:user/keys [id status] :as user} (database.user/lookup-by-telegram-chat-id user-telegram-chat-id (dl/db datomic-connection))]
     (when (= status :user.status/pending-activation)
       (database.user/activate! id datomic-connection))
     (if user
-      (do (morse-api/send-text (:token telegram)
-                               chat-id
-                               (str "O usuário foi ativado com sucesso."))
-          (morse-api/send-text (:token telegram)
-                               chat-id
-                               (str "A sua conta foi ativada.")))
-      (morse-api/send-text (:token telegram)
-                           chat-id
-                           (str "Usuário não encontrado")))))
+      (do (diplomat.telegram.producer/notify-user-activation! chat-id config)
+          (diplomat.telegram.producer/notify-user-activation! (:user/telegram-chat-id user) config))
+      (diplomat.telegram.producer/notify-user-not-found! chat-id config))))
--- src/ratatouille/diplomat/telegram/producer.clj
+++ src/ratatouille/diplomat/telegram/producer.clj
@@ -4,7 +4,21 @@
 
 (s/defn notify-user-creation!
   [telegram-chat-id :- s/Str
-   {:keys [telegram] :as config}]
+   {:keys [telegram]}]
   (morse-api/send-text (:token telegram)
                        telegram-chat-id
                        (str "Conta criada com sucesso, esse é o seu código de ativação: " telegram-chat-id)))
+
+(s/defn notify-user-activation!
+  [telegram-chat-id :- s/Str
+   {:keys [telegram]}]
+  (morse-api/send-text (:token telegram)
+                       telegram-chat-id
+                       "O usuário foi ativado com sucesso."))
+
+(s/defn notify-user-not-found!
+  [telegram-chat-id :- s/Str
+   {:keys [telegram]}]
+  (morse-api/send-text (:token telegram)
+                       telegram-chat-id
+                       "Usuário não encontrado"))
