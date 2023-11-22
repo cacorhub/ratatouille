789c335cdfc9b9a2240cdb8dffc3969152909362
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2023-11-22 07:39:08 -0300

    add skeleton to datomic config

--- src/ratatouille/db/datomic/config.clj
+++ src/ratatouille/db/datomic/config.clj
@@ -1,7 +1,9 @@
 (ns ratatouille.db.datomic.config
   (:require [ratatouille.wire.datomic.subscription :as wire.datomic.subscription]
-            [ratatouille.wire.datomic.user :as wire.datomic.user]))
+            [ratatouille.wire.datomic.user :as wire.datomic.user]
+            [ratatouille.wire.datomic.meal :as wire.datomic.meal]))
 
 (def schemas (concat []
                      wire.datomic.subscription/subscription-skeleton
-                     wire.datomic.user/user-skeleton))
+                     wire.datomic.user/user-skeleton
+                     wire.datomic.meal/meal-skeleton))
