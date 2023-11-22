553adf67d84b9258bdaa9a5ff51003ab339609aa
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2023-11-22 07:56:21 -0300

    add internal logic function in order to instantiate the meal internal entity

---
+++ src/ratatouille/logic/meal.clj
@@ -0,0 +1,14 @@
+(ns ratatouille.logic.meal
+  (:require [clj-time.core :as t]
+            [ratatouille.models.meal :as models.meal]
+            []
+            [schema.core :as s])
+  (:import (java.time LocalDate)))
+
+(s/defn ->meal :- models.meal/Meal
+  [reference-date :- LocalDate
+   type :- models.meal/Type]
+  {:meal/id             (random-uuid)
+   :meal/reference-date reference-date
+   :meal/created-at     (t/now)
+   :meal/type           type})
