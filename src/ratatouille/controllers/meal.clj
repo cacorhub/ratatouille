c7be88de54f418a2da206b079b86bd50fa19b396
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2023-11-23 05:47:33 -0300

    add weekday check before creating the meal

--- src/ratatouille/controllers/meal.clj
+++ src/ratatouille/controllers/meal.clj
@@ -1,5 +1,6 @@
 (ns ratatouille.controllers.meal
   (:require [clj-time.core :as t]
+            [clj-time.predicates :as t-predicates]
             [datomic.client.api :as dl]
             [schema.core :as s]
             [ratatouille.db.datomic.meal :as database.meal]
@@ -11,11 +12,11 @@
   (let [reference-date (t/today)
         today-lunch-meal (database.meal/by-reference-date-with-type reference-date :meal.type/lunch (dl/db datomic-connection))
         today-dinner-meal (database.meal/by-reference-date-with-type reference-date :meal.type/dinner (dl/db datomic-connection))]
-    (when-not today-lunch-meal
+    (when (and (not today-lunch-meal) (t-predicates/weekday? (t/now)))
       (-> (logic.meal/->meal reference-date :meal.type/lunch)
           (database.meal/insert! datomic-connection))
       (log/info ::lunch-meal-added-to-database))
-    (when-not today-dinner-meal
+    (when (and (not today-dinner-meal) (t-predicates/weekday? (t/now)))
       (-> (logic.meal/->meal reference-date :meal.type/dinner)
           (database.meal/insert! datomic-connection))
       (log/info ::dinner-meal-added-to-database))))
