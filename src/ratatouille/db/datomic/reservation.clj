36d7c18db307905ae9f3c2612aadb0f3c945095e
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2023-11-25 06:40:20 -0300

    lookup by meal

--- src/ratatouille/db/datomic/reservation.clj
+++ src/ratatouille/db/datomic/reservation.clj
@@ -18,3 +18,12 @@
                   [?reservation :reservation/user-id ?user-id]] datomic-db meal-id user-id)
           ffirst
           (dissoc :db/id)))
+
+(s/defn by-meal :- (s/maybe models.reservation/Reservation)
+  [meal-id :- s/Uuid
+   datomic-db]
+  (some-> (dl/q '[:find (pull ?reservation [*])
+                  :in $ ?meal-id
+                  :where [?reservation :reservation/meal-id ?meal-id]] datomic-db meal-id)
+          ffirst
+          (dissoc :db/id)))
