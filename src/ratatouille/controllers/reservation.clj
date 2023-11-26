65f3c9598bd8676397b736965c9bb2a12afaa034
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2023-11-26 09:31:09 -0300

    refactors controllers

--- src/ratatouille/controllers/reservation.clj
+++ src/ratatouille/controllers/reservation.clj
@@ -10,17 +10,12 @@
             [clj.qrgen])
   (:import (java.time LocalTime)))
 
-(s/defn with-in-time-window-for-lunch-reservation? :- s/Bool
-  [now :- LocalTime]
-  (and (jt/after? now (jt/local-time 6 0))
-       (jt/before? now (jt/local-time 12 0))))
-
 (s/defn reserve-lunch!
   [chat-id :- s/Str
    as-of :- LocalTime
    datomic-connection
    config]
-  (if (with-in-time-window-for-lunch-reservation? (jt/local-time as-of))
+  (if (logic.reservation/with-in-time-window-for-lunch-reservation? (jt/local-time as-of))
     (let [{user-id :user/id} (database.user/lookup-by-telegram-chat-id (str chat-id) (dl/db datomic-connection))
           {meal-id :meal/id} (database.meal/by-reference-date-with-type (jt/local-date as-of) :meal.type/lunch (dl/db datomic-connection))
           reservation (if-let [existent-reservation (database.reservation/by-meal-with-user meal-id user-id (dl/db datomic-connection))]
@@ -31,17 +26,12 @@
       (diplomat.telegram.producer/notify-reservation-qr-code! chat-id qr-code-file config))
     (diplomat.telegram.producer/notify-lunch-reservation-outside-time-window! chat-id config)))
 
-(s/defn with-in-time-window-for-dinner-reservation? :- s/Bool
-  [now :- LocalTime]
-  (and (jt/after? now (jt/local-time 12 0))
-       (jt/before? now (jt/local-time 16 0))))
-
 (s/defn reserve-dinner!
   [chat-id :- s/Str
    as-of :- LocalTime
    datomic-connection
    config]
-  (if (with-in-time-window-for-dinner-reservation? (jt/local-time as-of))
+  (if (logic.reservation/with-in-time-window-for-dinner-reservation? (jt/local-time as-of))
     (let [{:user/keys [id]} (database.user/lookup-by-telegram-chat-id (str chat-id) (dl/db datomic-connection))
           {meal-id :meal/id} (database.meal/by-reference-date-with-type (jt/local-date as-of) :meal.type/dinner (dl/db datomic-connection))
           reservation (if-let [existent-reservation (database.reservation/by-meal-with-user meal-id id (dl/db datomic-connection))]
--- src/ratatouille/logic/reservation.clj
+++ src/ratatouille/logic/reservation.clj
@@ -1,8 +1,11 @@
 (ns ratatouille.logic.reservation
-  (:require [ratatouille.models.meal :as models.meal]
+  (:require [common-clj.keyword.core :as common-keyword]
+            [java-time.api :as jt]
             [schema.core :as s]
-            [ratatouille.models.reservation :as models.reservation])
-  (:import (java.util Date)))
+            [ratatouille.models.reservation :as models.reservation]
+            [ratatouille.models.meal :as models.meal])
+  (:import (java.time LocalTime)
+           (java.util Date)))
 
 (s/defn ->reservation :- models.reservation/Reservation
   [meal-id :- s/Uuid
@@ -12,3 +15,19 @@
    :reservation/meal-id    meal-id
    :reservation/created-at (Date.)
    :reservation/status     :reservation.status/ready})
+
+(s/defn with-in-time-window-for-meal-reservation? :- s/Bool
+  [meal-type :- models.meal/Type
+   now :- LocalTime
+   config]
+  (let [meal-type' (common-keyword/un-namespaced meal-type)
+        begin-time-window (-> (:meal config) meal-type' :reservation-start)
+        end-time-window (-> (:meal config) meal-type' :reservation-end)]
+    (and (jt/after? now (jt/local-time begin-time-window))
+         (jt/before? now (jt/local-time end-time-window)))))
+
+(def with-in-time-window-for-lunch-reservation?
+  (partial with-in-time-window-for-meal-reservation? :meal.type/lunch))
+
+(def with-in-time-window-for-dinner-reservation?
+  (partial with-in-time-window-for-meal-reservation? :meal.type/dinner))
