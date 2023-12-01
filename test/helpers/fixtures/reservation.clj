f3cb695e2ac22071de390c4fdbc112eb8cea26c0
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2023-12-01 05:43:38 -0300

    add unit test datomic-to-internal-test

--- test/helpers/fixtures/reservation.clj
+++ test/helpers/fixtures/reservation.clj
@@ -1,13 +1,24 @@
 (ns fixtures.reservation
-  (:require
-   [fixtures.meal]
-   [java-time.api :as jt]))
+  (:require [fixtures.meal]
+            [java-time.api :as jt]))
 
 (def reservation-id (random-uuid))
 (def created-at (jt/instant))
+(def redeemed-at (jt/instant))
 
 (def reservation-ready
   {:reservation/id         reservation-id
    :reservation/meal-id    fixtures.meal/meal-id
    :reservation/created-at created-at
    :reservation/status     :reservation.status/ready})
+
+(def reservation-redeemed
+  (assoc reservation-ready :reservation/redeemed-at redeemed-at
+                           :reservation/status :reservation.status/redeemed))
+
+(def reservation-ready-datomic
+  (assoc reservation-ready :reservation/created-at (jt/java-date created-at)))
+
+(def reservation-redeemed-datomic
+  (assoc reservation-ready-datomic :reservation/redeemed-at (jt/java-date redeemed-at)
+                                   :reservation/status :reservation.status/redeemed))
--- test/unit/ratatouille/adapters/reservation_test.clj
+++ test/unit/ratatouille/adapters/reservation_test.clj
@@ -1,5 +1,6 @@
 (ns ratatouille.adapters.reservation-test
   (:require [clojure.test :refer :all]
+            [java-time.api :as jt]
             [matcher-combinators.test :refer [match?]]
             [ratatouille.adapters.reservation :as adapters.reservation]
             [schema.test :as s]
@@ -9,7 +10,29 @@
 (s/deftest internal-to-datomic-test
   (testing "Given a internal reservation entity we should be able to convert it to datomic model"
     (is (match? {:reservation/created-at inst?
-            :reservation/id         fixtures.reservation/reservation-id
-            :reservation/meal-id    fixtures.meal/meal-id
-            :reservation/status     :reservation.status/ready}
-           (adapters.reservation/internal->datomic fixtures.reservation/reservation-ready)))))
+                 :reservation/id         fixtures.reservation/reservation-id
+                 :reservation/meal-id    fixtures.meal/meal-id
+                 :reservation/status     :reservation.status/ready}
+                (adapters.reservation/internal->datomic fixtures.reservation/reservation-ready)))
+
+    (is (match? {:reservation/created-at  inst?
+                 :reservation/redeemed-at inst?
+                 :reservation/id          fixtures.reservation/reservation-id
+                 :reservation/meal-id     fixtures.meal/meal-id
+                 :reservation/status      :reservation.status/redeemed}
+                (adapters.reservation/internal->datomic fixtures.reservation/reservation-redeemed)))))
+
+(s/deftest datomic-to-internal-test
+  (testing "Given a datomic entity we can convert it to internal model"
+    (is (match? {:reservation/created-at jt/instant?
+                 :reservation/id         fixtures.reservation/reservation-id
+                 :reservation/meal-id    fixtures.meal/meal-id
+                 :reservation/status     :reservation.status/ready}
+                (adapters.reservation/datomic->internal fixtures.reservation/reservation-ready-datomic)))
+
+    (is (match? {:reservation/created-at  jt/instant?
+                 :reservation/id          fixtures.reservation/reservation-id
+                 :reservation/meal-id     fixtures.meal/meal-id
+                 :reservation/redeemed-at jt/instant?
+                 :reservation/status      :reservation.status/redeemed}
+                (adapters.reservation/datomic->internal fixtures.reservation/reservation-redeemed-datomic)))))
